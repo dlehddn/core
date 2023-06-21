7ce3450f4dc877caf73c96ec65f8be600aa5a9ca
woo <ldw28517@naver.com>
2023-06-21 10:14:42 +0900

    spirng-core

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,3 +1,5 @@
+package hello.core;
+
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
--- src/test/java/hello/core/scan/AutoAppConfigTest.java
+++ src/test/java/hello/core/scan/AutoAppConfigTest.java
@@ -1,5 +1,8 @@
 package hello.core.scan;
 
+import hello.core.AutoAppConfig;
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -9,5 +12,7 @@ public class AutoAppConfigTest {
     void basicScan() {
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
 
+        MemberService memberService = ac.getBean(MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
     }
 }
